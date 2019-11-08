49b89884ff727032a32953e25daa113aa2147a68
Diego Rojas <diego.gustavo.rojas@gmail.com>
2019-11-08 10:32:49 -0800

    Update initial business logic on scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,26 @@ function Pizza (size, toppings) {
   this.price = 0
 }
 
+Pizza.prototype.getPrice = function() {
+  this.toppings.forEach(function(toppings) {
+    this.price += 1;
+    if (this.size === small) {
+        this.price +=10
+      } else if (this.size === medium) {
+        this.price +=12;
+      } else {
+        this.price +=15
+      }
+    });
+    return this.price
+}
+
 
 
 
 
 // user logic
 $(document).ready(function() {
-    event.preventDefault();
+    // event.preventDefault();
 
 });
