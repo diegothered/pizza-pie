fd58b3811be4dacbb7dab8c07c6ca026890fd24c
Diego Rojas <diego.gustavo.rojas@gmail.com>
2019-11-11 12:35:48 -0800

    remove console logs used to fix bugs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 // business logic
 function Pizza (size, toppings) {
-  this.size = size
-  this.toppings = toppings
-  this.price = 0
+  this.size = size;
+  this.toppings = toppings;
+  this.price = 0;
 }
 
 Pizza.prototype.getPrice = function() {
@@ -12,13 +12,13 @@ Pizza.prototype.getPrice = function() {
       this.price += 10;
     } else if (this.size === "medium") {
       this.price += 12;
-      consol.log(this.price);
     } else {
       this.price += 15;
     }
-  });
-  return this.price
+  })
+  return this.price;
 }
+
 
 // user logic
 $(document).ready(function() {
@@ -26,19 +26,17 @@ $(document).ready(function() {
   $("form#input").submit(function(event) {
     event.preventDefault();
 
-
     var toppingInputs = $(".toppings:checked");
-    (console.log(toppingInputs))
     var toppingsArray = [];
     var sizeInput = $("#size").val();
-    console.log(sizeInput);
     toppingInputs.each(function(toppings) {
       toppingsArray.push(toppingInputs.val());
-      console.log(toppingsArray)
     });
+
     var myPizza = new Pizza(sizeInput,toppingsArray);
 
     var price = myPizza.getPrice();
-    console.log(price)
+
+    $("#show-price").text("The price of your pizza is $" + price + ".");
   });
 });
